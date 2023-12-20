093e8a10c9e9d2c3cf54c395df299722d95b76b1
cgtyeo <cgtyeo@gmail.com>
2023-12-20 13:53:11 +0300

    input validation added

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -15,13 +15,23 @@ const defaultFormState = {
 
 const Form = ({ setLocation }) => {
   const [formState, setFormState] = useState(defaultFormState);
-  console.log(formState);
   const [success, setSuccess] = useState(false);
 
   const [isChecked, setIsChecked] = useState(true);
+  const [isCheckedSecond, setIsCheckedSecond] = useState(true);
+  const [isCheckedThird, setIsCheckedThird] = useState(true);
 
-  const handleCheckboxChange = () => {
-    setIsChecked(!isChecked);
+  const handleCheckboxChange = (num, value) => {
+    console.log(num, value);
+    if (num === 1) {
+      setIsChecked(!value);
+    }
+    if (num === 2) {
+      setIsCheckedSecond(!value);
+    }
+    if (num === 3) {
+      setIsCheckedThird(!value);
+    }
   };
 
   const [isTooltipVisible, setIsTooltipVisible] = useState(false);
@@ -47,27 +57,28 @@ const Form = ({ setLocation }) => {
   const handleSubmit = (e) => {
     e.preventDefault();
     let hasErrors = handleFormValidations();
-    console.log("click");
+
     if (hasErrors) {
       setSuccess(false);
+
       return;
     }
     setSuccess(true);
+    setLocation("ResultPage");
   };
 
   const handleFormValidations = () => {
     let updatedState = { ...formState };
     let error = false;
 
-    const { tcNo, name, lastName, birthDate, gsmNo, email, ibanInfo, ibanNo } =
+    const { tcNo, name, lastName, gsmNo, email, ibanInfo, ibanNo } =
       updatedState;
 
     const lengthOftcNo = tcNo.value?.length;
-
     if (
       lengthOftcNo !== 11 ||
-      tcNo[lengthOftcNo - 2] % 2 !== 0 ||
-      tcNo[lengthOftcNo - 1] % 2 !== 0
+      parseInt(tcNo.value.charAt(10)) % 2 !== 0 ||
+      parseInt(tcNo.value.charAt(9)) % 2 !== 0
     ) {
       updatedState.tcNo.error = "Geçersiz TC No";
       error = true;
@@ -77,10 +88,9 @@ const Form = ({ setLocation }) => {
       error = true;
     }
     if (!lastName?.value) {
-      updatedState.lastName.error = "lastName cannot be empty";
+      updatedState.lastName.error = "Soy isim boş bırakılamaz";
       error = true;
     }
-
     const re =
       /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
 
@@ -88,12 +98,6 @@ const Form = ({ setLocation }) => {
       updatedState.email.error = "Geçersiz Email Adresi";
       error = true;
     }
-
-    if (1900 < !birthDate?.value < 2020) {
-      updatedState.birthDate.error = "Geçersiz Yıl";
-      error = true;
-    }
-
     setFormState({
       ...formState,
       ...updatedState,
@@ -104,7 +108,7 @@ const Form = ({ setLocation }) => {
   return (
     <div className="form-content-wrapper">
       <button className="btn-back" onClick={() => setLocation("ResultPage")}>
-        R
+        {"<"}
       </button>
       <div className="form-content">
         <div className="header-content">
@@ -116,208 +120,213 @@ const Form = ({ setLocation }) => {
             bilgilerinizi eksiksiz bir şekilde doldurunuz.
           </p>
         </div>
-        <form>
-          <div className="form-column-wrapper">
-            <div className="form-element">
-              <label className="label text-small-grey">
-                TC Kimlik Numarası
-              </label>
-              <input
-                type="text"
-                className="input text-medium-black"
-                value={formState?.tcNo?.value}
-                onChange={(e) => onChangeHandler("tcNo", e.target.value)}
-                placeholder="12345678912"
-              />
-              {formState?.tcNo?.error && (
-                <small className="error">{formState.tcNo.error}</small>
-              )}
-            </div>
-            <div className="form-element">
-              <label className="label text-small-grey">Ad</label>
-              <input
-                type="text"
-                className="input text-medium-black"
-                value={formState?.name?.value}
-                onChange={(e) => onChangeHandler("name", e.target.value)}
-                placeholder="Başak"
-              />
-              {formState?.name?.error && (
-                <small className="error">{formState.name.error}</small>
-              )}
-            </div>
-            <div className="form-element">
-              <label className="label text-small-grey">Soyad</label>
-              <input
-                type="text"
-                className="input text-medium-black"
-                value={formState?.lastName?.value}
-                onChange={(e) => onChangeHandler("lastName", e.target.value)}
-                placeholder="Yılmaz"
-              />
-              {formState?.lastName?.error && (
-                <small className="error">{formState.lastName.error}</small>
-              )}
-            </div>
-            <div className="form-element">
-              <label className="label text-small-grey">Doğum Yılı</label>
-              <input
-                type="text"
-                className="input text-medium-black"
-                value={formState?.birthDate?.value}
-                onChange={(e) => onChangeHandler("birthDate", e.target.value)}
-                placeholder="1990"
-              />
-              {formState?.birthDate?.error && (
-                <small className="error">{formState.birthDate.error}</small>
-              )}
-            </div>
-          </div>
-          <div className="form-column-wrapper">
-            <div className="form-element">
-              <label className="label text-small-grey">GSM Numarası</label>
-              <input
-                type="text"
-                className="input text-medium-black"
-                value={formState?.gsmNo?.value}
-                onChange={(e) => onChangeHandler("gsmNo", e.target.value)}
-                placeholder="(532) 532 1881"
-              />
-              {formState?.gsmNo?.error && (
-                <small className="error">{formState.gsmNo.error}</small>
-              )}
-            </div>
-            <div className="form-element">
-              <label className="label text-small-grey">E-posta Adresi</label>
-              <input
-                type="text"
-                className="input text-medium-black"
-                value={formState?.email?.value}
-                onChange={(e) => onChangeHandler("email", e.target.value)}
-                placeholder="basak.yilmaz@gmail.com"
-              />
-              {formState?.email?.error && (
-                <small className="error">{formState.email.error}</small>
-              )}
-            </div>
-            <div className="form-element">
-              <label className="label text-small-grey">
-                IBAN Sahibi Ad Soyad
-              </label>
-              <input
-                type="text"
-                className="input text-medium-black"
-                value={formState?.ibanInfo?.value}
-                onChange={(e) => onChangeHandler("ibanInfo", e.target.value)}
-                placeholder="Başak Yılmaz"
-              />
-              {formState?.ibanInfo?.error && (
-                <small className="error">{formState.ibanInfo.error}</small>
-              )}
-            </div>
-            <div className="form-element-info">
-              <div className="iban-content">
+        <form onSubmit={handleSubmit}>
+          <div className="form-input-wrapper">
+            <div className="form-column-wrapper">
+              <div className="form-element">
                 <label className="label text-small-grey">
-                  Paranın Yatırılacağı IBAN Numarası
+                  TC Kimlik Numarası*
                 </label>
                 <input
                   type="text"
                   className="input text-medium-black"
-                  value={formState?.ibanNo?.value}
-                  onChange={(e) => onChangeHandler("ibanNo", e.target.value)}
-                  placeholder="TR12 3456 7890 1234 5678"
+                  value={formState?.tcNo?.value}
+                  onChange={(e) => onChangeHandler("tcNo", e.target.value)}
+                  placeholder="12345678912"
                 />
+                {formState?.tcNo?.error && (
+                  <small className="error">{formState.tcNo.error}</small>
+                )}
               </div>
-
-              <div
-                className="info-icon"
-                onMouseOver={handleMouseOver}
-                onMouseOut={handleMouseOut}
-              >
-                <img src="assets/svg/info.svg" alt="info icon" />
-                <div className={isTooltipVisible ? "tooltip" : "hiddenTooltip"}>
-                  Teklifi kabul etmeniz durumunda belirlenen ücretin hesabınıza
-                  yatırılması için IBAN numarınızı paylaşmanız gerekmektedir.
+              <div className="form-element">
+                <label className="label text-small-grey">Ad*</label>
+                <input
+                  type="text"
+                  className="input text-medium-black"
+                  value={formState?.name?.value}
+                  onChange={(e) => onChangeHandler("name", e.target.value)}
+                  placeholder="Başak"
+                />
+                {formState?.name?.error && (
+                  <small className="error">{formState.name.error}</small>
+                )}
+              </div>
+              <div className="form-element">
+                <label className="label text-small-grey">Soyad*</label>
+                <input
+                  type="text"
+                  className="input text-medium-black"
+                  value={formState?.lastName?.value}
+                  onChange={(e) => onChangeHandler("lastName", e.target.value)}
+                  placeholder="Yılmaz"
+                />
+                {formState?.lastName?.error && (
+                  <small className="error">{formState.lastName.error}</small>
+                )}
+              </div>
+              <div className="form-element">
+                <label className="label text-small-grey">Doğum Yılı</label>
+                <input
+                  type="text"
+                  className="input text-medium-black"
+                  value={formState?.birthDate?.value}
+                  onChange={(e) => onChangeHandler("birthDate", e.target.value)}
+                  placeholder="1990"
+                />
+                {formState?.birthDate?.error && (
+                  <small className="error">{formState.birthDate.error}</small>
+                )}
+              </div>
+            </div>
+            <div className="form-column-wrapper">
+              <div className="form-element">
+                <label className="label text-small-grey">GSM Numarası</label>
+                <input
+                  type="text"
+                  className="input text-medium-black"
+                  value={formState?.gsmNo?.value}
+                  onChange={(e) => onChangeHandler("gsmNo", e.target.value)}
+                  placeholder="(532) 532 1881"
+                />
+                {formState?.gsmNo?.error && (
+                  <small className="error">{formState.gsmNo.error}</small>
+                )}
+              </div>
+              <div className="form-element">
+                <label className="label text-small-grey">E-posta Adresi*</label>
+                <input
+                  type="text"
+                  className="input text-medium-black"
+                  value={formState?.email?.value}
+                  onChange={(e) => onChangeHandler("email", e.target.value)}
+                  placeholder="basak.yilmaz@gmail.com"
+                />
+                {formState?.email?.error && (
+                  <small className="error">{formState.email.error}</small>
+                )}
+              </div>
+              <div className="form-element">
+                <label className="label text-small-grey">
+                  IBAN Sahibi Ad Soyad
+                </label>
+                <input
+                  type="text"
+                  className="input text-medium-black"
+                  value={formState?.ibanInfo?.value}
+                  onChange={(e) => onChangeHandler("ibanInfo", e.target.value)}
+                  placeholder="Başak Yılmaz"
+                />
+                {formState?.ibanInfo?.error && (
+                  <small className="error">{formState.ibanInfo.error}</small>
+                )}
+              </div>
+              <div className="form-element-info">
+                <div className="iban-content">
+                  <label className="label text-small-grey">
+                    Paranın Yatırılacağı IBAN Numarası
+                  </label>
+                  <input
+                    type="text"
+                    className="input text-medium-black"
+                    value={formState?.ibanNo?.value}
+                    onChange={(e) => onChangeHandler("ibanNo", e.target.value)}
+                    placeholder="TR12 3456 7890 1234 5678"
+                  />
+                </div>
+                <div
+                  className="info-icon"
+                  onMouseOver={handleMouseOver}
+                  onMouseOut={handleMouseOut}
+                >
+                  <img src="assets/svg/info.svg" alt="info icon" />
+                  <div
+                    className={isTooltipVisible ? "tooltip" : "hiddenTooltip"}
+                  >
+                    Teklifi kabul etmeniz durumunda belirlenen ücretin
+                    hesabınıza yatırılması için IBAN numarınızı paylaşmanız
+                    gerekmektedir.
+                  </div>
                 </div>
               </div>
+
               {formState?.ibanNo?.error && (
                 <small className="error">{formState.ibanNo.error}</small>
               )}
             </div>
           </div>
+          <div className="checkbox-wrapper">
+            <div className="checkbox-content">
+              <input
+                type="checkbox"
+                name="checkbox1"
+                id="customCheckbox"
+                checked={isChecked}
+                onChange={(e) => handleCheckboxChange(1, e.target.checked)}
+              />
+
+              <label
+                htmlFor="customCheckbox"
+                className={isChecked ? `labelChecked` : `labelUnChecked`}
+              >
+                <a href="#" className="text-a-blue">
+                  Cihaz Aydınlatma Metnini
+                </a>{" "}
+                <span className="text-info-small-grey">
+                  okudum ve onaylıyorum.
+                </span>
+              </label>
+            </div>
+            <div className="checkbox-content">
+              <input
+                type="checkbox"
+                name="checkbox1"
+                id="customCheckbox"
+                checked={isCheckedSecond}
+                onChange={(e) => handleCheckboxChange(2, e.target.checked)}
+              />
+
+              <label
+                htmlFor="customCheckbox"
+                className={isCheckedSecond ? `labelChecked` : `labelUnChecked`}
+              >
+                <a href="#" className="text-a-blue">
+                  Ticari bilgilendirme Sözleşmesini
+                </a>{" "}
+                <span className="text-info-small-grey">
+                  okudum ve onaylıyorum.
+                </span>
+              </label>
+            </div>
+            <div className="checkbox-content">
+              <input
+                type="checkbox"
+                name="checkbox1"
+                id="customCheckbox"
+                checked={isCheckedThird}
+                onChange={(e) => handleCheckboxChange(3, e.target.checked)}
+              />
+
+              <label
+                htmlFor="customCheckbox"
+                className={isCheckedThird ? `labelChecked` : `labelUnChecked`}
+              >
+                <a href="#" className="text-a-blue">
+                  Geri Alım Sözleşmesini
+                </a>{" "}
+                <span className="text-info-small-grey">
+                  okudum ve onaylıyorum.
+                </span>
+              </label>
+            </div>
+          </div>
+          <button
+            className="btn-primary btn-form"
+            type="submit"
+            onSubmit={handleSubmit}
+          >
+            Geri Alım Talebini Tamamla
+          </button>
         </form>
-        <div className="checkbox-wrapper">
-          <div className="checkbox-content">
-            <input
-              type="checkbox"
-              name="checkbox1"
-              id="customCheckbox"
-              checked={isChecked}
-              onChange={handleCheckboxChange}
-            />
-
-            <label
-              htmlFor="customCheckbox"
-              className={isChecked ? `labelChecked` : `labelUnChecked`}
-            >
-              <a href="#" className="text-a-blue">
-                Cihaz Aydınlatma Metnini
-              </a>{" "}
-              <span className="text-info-small-grey">
-                okudum ve onaylıyorum.
-              </span>
-            </label>
-          </div>
-          <div className="checkbox-content">
-            <input
-              type="checkbox"
-              name="checkbox1"
-              id="customCheckbox"
-              checked={isChecked}
-              onChange={handleCheckboxChange}
-            />
-
-            <label
-              htmlFor="customCheckbox"
-              className={isChecked ? `labelChecked` : `labelUnChecked`}
-            >
-              <a href="#" className="text-a-blue">
-                Ticari bilgilendirme Sözleşmesini
-              </a>{" "}
-              <span className="text-info-small-grey">
-                okudum ve onaylıyorum.
-              </span>
-            </label>
-          </div>
-          <div className="checkbox-content">
-            <input
-              type="checkbox"
-              name="checkbox1"
-              id="customCheckbox"
-              checked={isChecked}
-              onChange={handleCheckboxChange}
-            />
-
-            <label
-              htmlFor="customCheckbox"
-              className={isChecked ? `labelChecked` : `labelUnChecked`}
-            >
-              <a href="#" className="text-a-blue">
-                Geri Alım Sözleşmesini
-              </a>{" "}
-              <span className="text-info-small-grey">
-                okudum ve onaylıyorum.
-              </span>
-            </label>
-          </div>
-        </div>
-        <button
-          className="btn-primary btn-form"
-          type="submit"
-          onSubmit={handleSubmit}
-        >
-          Geri Alım Talebini Tamamla
-        </button>
       </div>
       <TradeInfo />
     </div>
